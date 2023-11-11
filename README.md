036a6e494a4f1cc0004e28f10355976aaae4dea6
Esteban Casallas <ecasallas07@gmail.com>
2023-11-10 23:12:03 -0500

    Creation API complete

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # API-REST
 *Programming concepts, in this case with practical code, the API concept*
 ## Iterface Programming Aplication 
+    The API also allows us to facilitate access to data or resources that have already been designed, with the API being an interface that allows us to reuse that functionality to access those resources or data that have already been previously established, which boils down to not reinventing the wheel.
+**They do not need to know internal details of the other applications, which is a great advantage of the API, they work between different software.**
 
+### Characteristics:
+- *Modularity:* Property that allows an application to be subdivided into smaller parts (called modules), each of which must be as independent as possible from the application itself and from the remaining parts.
+- *Scalabilty:* Software scalability refers to its ability to handle an increasing load of data and work, in other words, the ability of a system to continue meeting the needs of your business as it grows.
--- model/data.php
+++ model/data.php
@@ -9,34 +9,39 @@ header('Content-Type: application/json');
 //De esta forma estamos viendo que tipo de peticion recibe el server
 $method = $_SERVER['REQUEST_METHOD'];
 $conection = connection();
-// print_r($method);
+
+//Obtener parametro o id para eliminar
+//Server-> PATH_INFO contieneinformacion de la url
+$path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/' ;
+$buscar_id = explode('/',$path);
+$id = ($path !== '/') ? end($buscar_id):null;
 
 switch ($method) 
 {
     case 'GET':
-        methodGet($conection);
+        methodGet($conection,$id);
         break;
     case 'POST':
         insertPost($conection);
         break;    
     case 'PUT':
-        echo ' Modificar registros';
+        update($conection,$id);
         break;    
     case 'DELETE':
-            echo ' elimina registro';
-            break;
+        delete($conection,$id);
+        break;
     default:
             echo ' no se registro metodo';
 }
        
 
-function methodGet($conection)
+function methodGet($conection,$id=null)
 {
-
-    $query = $conection->query("SELECT * FROM usuarios");
+    $query = ($id===null) ? $conection->query("SELECT * FROM usuarios"): $conection->query("SELECT * FROM usuarios WHERE id = $id");
+   
  
     if($query){
-        $result = $query->fetch_assoc(); 
+        // $result = $query->fetch_assoc(); 
         // TODO: Las primeras formas comentadas, no funcionan porque envie el indice, duplicando el registro
         // $result = iterator_to_array($query, PDO::FETCH_OBJ);
         // $result = $query->fetchAll();
@@ -70,4 +75,40 @@ function methodGet($conection)
     // echo json_encode($query);
 
 
+ }
+
+ function delete($conection,$id)
+ {
+    $query = $conection->prepare('DELETE FROM usuarios WHERE id =:id');
+    $query->bindParam(':id',$id);
+    $query->execute();
+
+    if($query)
+    {
+        echo json_encode(['success'=>'The user was successfully deleted']);
+    }else{
+        echo json_encode(['error'=>'The user was not found']);
+    }
+ }
+
+ function update($conection,$id)
+ {
+    $data = json_decode(file_get_contents('php://input'),true);
+    $name = $data['name'];
+    $surname =$data['surname'];
+    $query = $conection->prepare('UPDATE usuarios SET nombre = :name, apellido = :surname WHERE id =:id');
+    $query->execute(
+        [
+            'name'=>$name,
+            'surname'=>$surname,
+            'id'=>$id
+        
+        ]
+    );
+    if($query)
+    {
+        echo json_encode(['success'=>'The user was successfully updated']);
+    }else{
+        echo json_encode(['error'=>'The user was not updated']);
+    }
  }
